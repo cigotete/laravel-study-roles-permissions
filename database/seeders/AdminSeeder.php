24e6f580b5a8b7c66cd5c161cadcdeff48088237
Camilo Figueroa <cigotete@gmail.com>
2023-02-03 18:41:31 -0500

    Created seeder for admin and user roles, and seed for Admin user. Also updated main Seed to indicate order execution seeders.

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $adminRole = Role::where('name', 'admin')->first();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456'),
+            'email_verified_at' => now(),
+            'role_id' => $adminRole->id,
+        ]);
+    }
+}
---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userRole = Role::create(['name' => 'user']);
+        $adminRole = Role::create(['name' => 'admin']);
+    }
+}

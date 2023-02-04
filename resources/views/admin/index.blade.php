851d00545e1e545c92d1bcbc9eca5fbdf9575895
Camilo Figueroa <cigotete@gmail.com>
2023-02-04 12:01:53 -0500

    Added Admin controller, and respective routing and views for Admin panel.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,3 @@
+<x-admin-layout>
+    <h1>admin/index blade.php file</h1>
+</x-admin-layout>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/admin', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('admin.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');

69f20dd3435dda940b347a64c6a1906b1ac4c907
Camilo Figueroa <cigotete@gmail.com>
2023-02-05 15:47:57 -0500

    Added middleware in order to check if user is Admin.

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/admin', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('admin.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth', 'is_admin')->group(function () {
+    Route::get('/admin', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('admin.index');
 });
 
 require __DIR__.'/auth.php';

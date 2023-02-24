31de8c3a949ce4ab506c457ac466d84d7b59dac2
Camilo Figueroa <cigotete@gmail.com>
2023-02-23 23:09:57 -0500

    Fix Post Create title.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,7 +3,7 @@
         @can('create', App\Models\Post::class)
             <div class="flex m-2 p-2">
                 <a href="{{ route('posts.index') }}" class="px-4 py-2 bg-indigo-400 hover:bg-indigo-600 rounded">
-                    Posts Index</a>
+                    Posts Create</a>
             </div>
         @endcan
         <div class="max-w-md mx-auto p-4">
